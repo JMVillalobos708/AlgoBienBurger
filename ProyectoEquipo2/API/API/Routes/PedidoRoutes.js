12c3c302a833225ec817245248eefb26c97cd252
JMVillalobos708 <84457999+JMVillalobos708@users.noreply.github.com>
2023-11-28 23:47:48 -0600

    Delete ProyectoEquipo2/API/API directory

--- ProyectoEquipo2/API/API/Routes/PedidoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const PedidoController = require('../Controllers/PedidoController')
-
-router.get('/pedido', PedidoController.Get);
-router.post('/pedido/:id', PedidoController.Post);
-
-module.exports = router;
