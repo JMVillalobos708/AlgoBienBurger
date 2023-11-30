c9f5d909f4b09fe7ee46510b2e4ba374219b2259
diegodev <andreelopez334@gmail.com>
2023-11-30 00:56:13 -0600

    arreglaDITTO

--- ProyectoEquipo2/src/js/generarReporteM.js
+++ ProyectoEquipo2/src/js/generarReporteM.js
@@ -2,29 +2,37 @@ import {LocalStorage} from './services/localstorage.js'
 import {Login} from './services/login.js'
 const _LocalStorage = new LocalStorage()
 const login = new Login();
-
+var reportesList = [];
+var tabla;
 async function construirYAgregarTabla(data) {
     try {
         //const datos = await obtenerPedidos();
+        if (!tabla){
+            //const datos = [{pagadas: 1, no_pagadas: 3, total_ventas: 4, resultado: 3, mes: 12, total: 5}]; 
+            const datos = [data];
+            console.info(datos);
+            const tablaContainer = document.getElementById('tablaReporte');
 
-        //const datos = [{pagadas: 1, no_pagadas: 3, total_ventas: 4, resultado: 3, mes: 12, total: 5}]; 
-        const datos = [data];
-        console.info(datos);
-        const tablaContainer = document.getElementById('tablaReporte');
+            
+            // Crear la tabla con simple-datatables
+            tabla = new simpleDatatables.DataTable(tablaContainer, {
+                data: {
+                    headings: ['Pagadas', 'No pagadas', 'Total Ventas', 'Resultado', 'Mes', 'Total'],
+                    data: datos.map(({ pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal }) => {
+                        
+                        return [pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal];
+                    }),
+                },
+                search: false,
 
-    
-        // Crear la tabla con simple-datatables
-        const tabla = new simpleDatatables.DataTable(tablaContainer, {
-            data: {
-                headings: ['Pagadas', 'No pagadas', 'Total Ventas', 'Resultado', 'Mes', 'Total'],
-                data: datos.map(({ pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal }) => {
-                    
-                    return [pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal];
-                }),
-            },
-            search: false,
-
-        });
+            });
+        }
+        else{
+            tabla.insert(datos.map(({ pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal }) => {
+                return [pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal];
+            }));
+        }
+       
         
         // Puedes personalizar la configuración según tus necesidades
         // Por ejemplo, si deseas habilitar la búsqueda:
@@ -59,6 +67,7 @@ async function generarReporte(){
     const response = await fetch("http://127.0.0.1:5200/api/reporte", configuration);
     const data = await response.json();
     console.log(data);
+    reportesList.push(data);
     construirYAgregarTabla(data);
 }
 // window.addEventListener('DOMContentLoaded', construirYAgregarTabla);
---
+++ ProyectoEquipo2/src/js/verReporteM.js
@@ -0,0 +1,71 @@
+import {LocalStorage} from './services/localstorage.js'
+import {Login} from './services/login.js'
+const _LocalStorage = new LocalStorage()
+const login = new Login();
+async function obtenerReportes() {
+    const url = 'https://apiburger.onrender.com/api/reporte';
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log('Respuesta del servidor:', data);
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Propaga el error para que pueda ser manejado en la función loadOntable
+    }
+}
+
+async function construirYAgregarTabla() {
+    try {
+        const datos = await obtenerReportes();
+
+        //const datos = [{id_cuenta: 1, nombre_p: 'caca', precio_p: 200, cantidad: 3, bol_pago: false, fecha: '2024/03/2024'}]; 
+
+        const tablaContainer = document.getElementById('datatablesSimple');
+    
+        // Crear la tabla con simple-datatables
+        const tabla = new simpleDatatables.DataTable(tablaContainer, {
+            data: {
+                headings: ['Pagadas', 'No pagadas', 'Total Ventas', 'Resultado', 'Mes', 'Total'],
+                data: datos.map(({ pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal }) => {
+                    
+                    return [pagadas, no_pagadas, total_ventas, resultado, mes, resultado_terminal];
+                }),
+            },
+
+        });
+        
+        // Puedes personalizar la configuración según tus necesidades
+        // Por ejemplo, si deseas habilitar la búsqueda:
+        // const tabla = new simpleDatatables.DataTable(tablaContainer, { search: true });
+        
+    
+    } catch (error) {
+        console.error('Error al construir y agregar la tabla:', error);
+    
+        const tablaContainer = document.getElementById('datatablesSimple');
+    
+        if (tablaContainer) {
+            new simpleDatatables.DataTable(tablaContainer);
+        }
+    }
+}
+
+window.addEventListener('DOMContentLoaded', construirYAgregarTabla);
+const user = JSON.parse(_LocalStorage.get("user"));
+if (user != null){
+    const user_entry = document.getElementById("user-entry");
+    console.log(user);
+    user_entry.innerHTML = user.username;
+}
+console.log("JS VINCULADO")
+
+const btnCerrarSesion = document.getElementById("btn-cerrar-sesion").addEventListener('click', login.cerrarSesion);
+
