d7ec35c4f541aff8950e01a5deb540f4ac5c3647
diegodev <andreelopez334@gmail.com>
2023-11-29 18:41:15 -0600

    Updates

--- ProyectoEquipo2/src/js/index.js
+++ ProyectoEquipo2/src/js/index.js
@@ -1,6 +1,6 @@
 //Codigo de Javascript para conectar index (logica del login) con el frontend;
-import {Login} from '../services/login.js' 
-import {LocalStorage} from '../services/localstorage.js'
+import {Login} from './services/login.js' 
+import {LocalStorage} from './services/localstorage.js'
 const btnIniciarSesion = document.getElementById("btnLogin");
 const btnDashBoard = document.getElementById("dashboard-btn");
 const btnLogIn = document.getElementById("login");
